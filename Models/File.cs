08bf91bb071e6c56d9d3aab7e900918bd908bfd3
fw2220 <fw2220@DESKTOP-JQUR058>
2025-07-10 13:03:33 -0500

    Add project files.

---
+++ Models/File.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadmansFileshareAppCSharp.Models
+{
+    internal class File
+    {
+    }
+}
