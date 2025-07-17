034e0e69a0610bd3adb768fdf3f1ed0195c1be28
tuccr <tuckerbcarroll.business@gmail.com>
2025-07-17 16:14:35 -0500

    created FileView class

---
+++ Models/FileView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace DeadmansFileshareAppCSharp.Models
+{
+    internal class FileView
+    {
+        public ObservableCollection<File> Files { get; set; } = new ObservableCollection<File>();
+
+        public FileView()
+        {
+        }
+    }
+}
