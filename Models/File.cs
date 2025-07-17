9e7620863682ca3bda2cd593685ccc481c537726
tuccr <tuckerbcarroll.business@gmail.com>
2025-07-17 16:14:06 -0500

    created File object

--- Models/File.cs
+++ Models/File.cs
@@ -8,5 +8,14 @@ namespace DeadmansFileshareAppCSharp.Models
 {
     internal class File
     {
+        private string filename { get; set; } = string.Empty;
+        private string userID { get; set; } = string.Empty;
+        private DateTime expirationDate { get; set; } = DateTime.MinValue;
+
+        public File(string filename, DateTime expirationDate)
+        {
+            this.filename = filename;
+            this.expirationDate = expirationDate;
+        }
     }
 }
