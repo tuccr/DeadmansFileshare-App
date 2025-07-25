7f4f97f5244fdb8c43452bd1ccbdc59ff1cc89bc
tuccr <tuckerbcarroll.business@gmail.com>
2025-07-25 14:28:25 -0500

    added functionality for loading files from backend

--- Models/FileView.cs
+++ Models/FileView.cs
@@ -2,19 +2,76 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
+using Windows.Security.Credentials;
+using Microsoft.Security;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using DeadmansFileshareAppCSharp.Views;
 
 namespace DeadmansFileshareAppCSharp.Models
 {
     internal class FileView
     {
-        public ObservableCollection<File> Files { get; set; } = new ObservableCollection<File>();
+        public ObservableCollection<File>? Files { get; set; }
 
         public FileView()
         {
+            LoadFiles();
         }
+
+        private async void LoadFiles()
+        {
+            String CRED_NAME = AppConfig.CRED_NAME;
+            String userName = AppConfig.userName;
+
+            if(String.IsNullOrEmpty(userName))
+            {
+                return;
+            }
+
+            var vault = new PasswordVault();
+            var cred = vault.Retrieve(CRED_NAME, userName);
+
+            if(cred == null)
+            {
+                // error, force logout
+                return;
+            }
+
+            cred.RetrievePassword();
+
+            await GetFiles(cred.Password);
+        }
+
+        private async Task<bool> GetFiles(String token)
+        {
+            String API_URI = AppConfig.API_URI;
+
+            using var client = new HttpClient();
+
+            var payload = new { token };
+            var content = new StringContent(JsonSerializer.Serialize(payload), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(API_URI + "/files/getAllFiles", content);
+
+            if(response.IsSuccessStatusCode)
+            {
+                String jsonResponse = await response.Content.ReadAsStringAsync();
+                Files = JsonSerializer.Deserialize<ObservableCollection<File>>(jsonResponse);
+                Console.WriteLine(jsonResponse);
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine($"Couldn't load files, sad");
+                return false;
+            }
+        }
+
     }
 }
